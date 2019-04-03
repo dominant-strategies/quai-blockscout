jackispm/blockscout

feat: verify contracts via an RPC endpoint

apps/block_scout_web/lib/block_scout_web/etherscan.ex
@@ -328,6 +328,49 @@ defmodule BlockScoutWeb.Etherscan do
     "result" => nil
   }
 
+  @contract_verify_example_value %{
+    "status" => "1",
+    "message" => "OK",
+    "result" => %{
+      "SourceCode" => """
+      pragma solidity >0.4.24;
+
+      contract Test {
+      constructor() public { b = hex"12345678901234567890123456789012"; }
+      event Event(uint indexed a, bytes32 b);
+      event Event2(uint indexed a, bytes32 b);
+      function foo(uint a) public { emit Event(a, b); }
+      bytes32 b;
+      }
+      """,
+      "ABI" => """
+      [{
+      "type":"event",
+      "inputs": [{"name":"a","type":"uint256","indexed":true},{"name":"b","type":"bytes32","indexed":false}],
+      "name":"Event"
+      }, {
+      "type":"event",
+      "inputs": [{"name":"a","type":"uint256","indexed":true},{"name":"b","type":"bytes32","indexed":false}],
+      "name":"Event2"
+      }, {
+      "type":"function",
+      "inputs": [{"name":"a","type":"uint256"}],
+      "name":"foo",
+      "outputs": []
+      }]
+      """,
+      "ContractName" => "Test",
+      "CompilerVersion" => "v0.2.1-2016-01-30-91a6b35",
+      "OptimizationUsed" => "1"
+    }
+  }
+
+  @contract_verify_example_value_error %{
+    "status" => "0",
+    "message" => "There was an error verifying the contract.",
+    "result" => nil
+  }
+
   @contract_getsourcecode_example_value %{
     "status" => "1",
     "message" => "OK",
@@ -1747,6 +1790,120 @@ defmodule BlockScoutWeb.Etherscan do
     ]
   }
 
+  @contract_verify_action %{
+    name: "verify",
+    description: "Verify a contract with its source code and contract creation information.",
+    required_params: [
+      %{
+        key: "addressHash",
+        placeholder: "addressHash",
+        type: "string",
+        description: "The address of the contract."
+      },
+      %{
+        key: "name",
+        placebolder: "name",
+        type: "string",
+        description: "The name of the contract."
+      },
+      %{
+        key: "compilerVersion",
+        placeholder: "compilerVersion",
+        type: "string",
+        description: "The compiler version for the contract."
+      },
+      %{
+        key: "optimization",
+        placeholder: false,
+        type: "boolean",
+        description: "Whether or not compiler optimizations were enabled."
+      },
+      %{
+        key: "contractSourceCode",
+        placeholder: "contractSourceCode",
+        type: "string",
+        description: "The source code of the contract."
+      }
+    ],
+    optional_params: [
+      %{
+        key: "constructorArguments",
+        type: "string",
+        description: "The constructor argument data provided."
+      },
+      %{
+        key: "evmVersion",
+        placeholder: "evmVersion",
+        type: "string",
+        description: "The EVM version for the contract."
+      },
+      %{
+        key: "library1Name",
+        type: "string",
+        description: "The name of the first library used."
+      },
+      %{
+        key: "library1Address",
+        type: "string",
+        description: "The address of the first library used."
+      },
+      %{
+        key: "library2Name",
+        type: "string",
+        description: "The name of the second library used."
+      },
+      %{
+        key: "library2Address",
+        type: "string",
+        description: "The address of the second library used."
+      },
+      %{
+        key: "library3Name",
+        type: "string",
+        description: "The name of the third library used."
+      },
+      %{
+        key: "library3Address",
+        type: "string",
+        description: "The address of the third library used."
+      },
+      %{
+        key: "library4Name",
+        type: "string",
+        description: "The name of the fourth library used."
+      },
+      %{
+        key: "library4Address",
+        type: "string",
+        description: "The address of the fourth library used."
+      },
+      %{
+        key: "library5Name",
+        type: "string",
+        description: "The name of the fourth library used."
+      },
+      %{
+        key: "library5Address",
+        type: "string",
+        description: "The address of the fourth library used."
+      }
+    ],
+    responses: [
+      %{
+        code: "200",
+        description: "successful operation",
+        example_value: Jason.encode!(@contract_verify_example_value),
+        type: "model",
+        model: @contract_model
+      },
+      %{
+        code: "200",
+        description: "error",
+        example_value: Jason.encode!(@contract_verify_example_value_error)
+      }
+    ]
+  }
+
   @contract_getabi_action %{
     name: "getabi",
     description: "Get ABI for verified contract. Also available through a GraphQL 'addresses' query.",
@@ -1976,7 +2133,8 @@ defmodule BlockScoutWeb.Etherscan do
     actions: [
       @contract_listcontracts_action,
       @contract_getabi_action,
-      @contract_getsourcecode_action
+      @contract_getsourcecode_action,
+      @contract_verify_action
     ]
   }
 

dominant-strategies/quai-blockscout

apps/block_scout_web/lib/block_scout_web/etherscan.ex
@@ -328,6 +328,49 @@ defmodule BlockScoutWeb.Etherscan do
     "result" => nil
   }
 
+  @contract_verify_example_value %{
+    "status" => "1",
+    "message" => "OK",
+    "result" => %{
+      "SourceCode" => """
+      pragma solidity >0.4.24;
+
+      contract Test {
+      constructor() public { b = hex"12345678901234567890123456789012"; }
+      event Event(uint indexed a, bytes32 b);
+      event Event2(uint indexed a, bytes32 b);
+      function foo(uint a) public { emit Event(a, b); }
+      bytes32 b;
+      }
+      """,
+      "ABI" => """
+      [{
+      "type":"event",
+      "inputs": [{"name":"a","type":"uint256","indexed":true},{"name":"b","type":"bytes32","indexed":false}],
+      "name":"Event"
+      }, {
+      "type":"event",
+      "inputs": [{"name":"a","type":"uint256","indexed":true},{"name":"b","type":"bytes32","indexed":false}],
+      "name":"Event2"
+      }, {
+      "type":"function",
+      "inputs": [{"name":"a","type":"uint256"}],
+      "name":"foo",
+      "outputs": []
+      }]
+      """,
+      "ContractName" => "Test",
+      "CompilerVersion" => "v0.2.1-2016-01-30-91a6b35",
+      "OptimizationUsed" => "1"
+    }
+  }
+
+  @contract_verify_example_value_error %{
+    "status" => "0",
+    "message" => "There was an error verifying the contract.",
+    "result" => nil
+  }
+
   @contract_getsourcecode_example_value %{
     "status" => "1",
     "message" => "OK",
@@ -1747,6 +1790,126 @@ defmodule BlockScoutWeb.Etherscan do
     ]
   }
 
+  @contract_verify_action %{
+    name: "verify",
+    description: "Verify a contract with its source code and contract creation information.",
+    required_params: [
+      %{
+        key: "addressHash",
+        placeholder: "addressHash",
+        type: "string",
+        description: "The address of the contract."
+      },
+      %{
+        key: "name",
+        placeholder: "name",
+        type: "string",
+        description: "The name of the contract."
+      },
+      %{
+        key: "compilerVersion",
+        placeholder: "compilerVersion",
+        type: "string",
+        description: "The compiler version for the contract."
+      },
+      %{
+        key: "optimization",
+        placeholder: false,
+        type: "boolean",
+        description: "Whether or not compiler optimizations were enabled."
+      },
+      %{
+        key: "contractSourceCode",
+        placeholder: "contractSourceCode",
+        type: "string",
+        description: "The source code of the contract."
+      }
+    ],
+    optional_params: [
+      %{
+        key: "constructorArguments",
+        type: "string",
+        description: "The constructor argument data provided."
+      },
+      %{
+        key: "evmVersion",
+        placeholder: "evmVersion",
+        type: "string",
+        description: "The EVM version for the contract."
+      },
+      %{
+        key: "optimizationRuns",
+        placeholder: "optimizationRuns",
+        type: "integer",
+        description: "The number of optimization runs used during compilation"
+      },
+      %{
+        key: "library1Name",
+        type: "string",
+        description: "The name of the first library used."
+      },
+      %{
+        key: "library1Address",
+        type: "string",
+        description: "The address of the first library used."
+      },
+      %{
+        key: "library2Name",
+        type: "string",
+        description: "The name of the second library used."
+      },
+      %{
+        key: "library2Address",
+        type: "string",
+        description: "The address of the second library used."
+      },
+      %{
+        key: "library3Name",
+        type: "string",
+        description: "The name of the third library used."
+      },
+      %{
+        key: "library3Address",
+        type: "string",
+        description: "The address of the third library used."
+      },
+      %{
+        key: "library4Name",
+        type: "string",
+        description: "The name of the fourth library used."
+      },
+      %{
+        key: "library4Address",
+        type: "string",
+        description: "The address of the fourth library used."
+      },
+      %{
+        key: "library5Name",
+        type: "string",
+        description: "The name of the fourth library used."
+      },
+      %{
+        key: "library5Address",
+        type: "string",
+        description: "The address of the fourth library used."
+      }
+    ],
+    responses: [
+      %{
+        code: "200",
+        description: "successful operation",
+        example_value: Jason.encode!(@contract_verify_example_value),
+        type: "model",
+        model: @contract_model
+      },
+      %{
+        code: "200",
+        description: "error",
+        example_value: Jason.encode!(@contract_verify_example_value_error)
+      }
+    ]
+  }
+
   @contract_getabi_action %{
     name: "getabi",
     description: "Get ABI for verified contract. Also available through a GraphQL 'addresses' query.",
@@ -1976,7 +2139,8 @@ defmodule BlockScoutWeb.Etherscan do
     actions: [
       @contract_listcontracts_action,
       @contract_getabi_action,
-      @contract_getsourcecode_action
+      @contract_getsourcecode_action,
+      @contract_verify_action
     ]
   }
 