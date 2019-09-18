DappGuy/blockscout

Add ETS-based cache for accounts page

apps/explorer/lib/explorer/chain.ex
@@ -48,6 +48,7 @@ defmodule Explorer.Chain do
   alias Explorer.Chain.Block.{EmissionReward, Reward}
 
   alias Explorer.Chain.Cache.{
+    Accounts,
     BlockCount,
     BlockNumber,
     Blocks,
@@ -1375,6 +1376,36 @@ defmodule Explorer.Chain do
   def list_top_addresses(options \\ []) do
     paging_options = Keyword.get(options, :paging_options, @default_paging_options)
 
+    if is_nil(paging_options.key) do
+      paging_options.page_size
+      |> Accounts.take_enough()
+      |> case do
+        nil ->
+          accounts_with_n = fetch_top_addresses(paging_options)
+
+          accounts_with_n
+          |> Enum.map(fn {address, _n} -> address end)
+          |> Accounts.drop_or_update()
+
+          accounts_with_n
+
+        accounts ->
+          Enum.map(
+            accounts,
+            &{&1,
+             if &1.nonce do
+               &1.nonce + 1
+             else
+               0
+             end}
+          )
+      end
+    else
+      fetch_top_addresses(paging_options)
+    end
+  end
+
+  defp fetch_top_addresses(paging_options) do
     base_query =
       from(a in Address,
         where: a.fetched_coin_balance > ^0,

apps/indexer/lib/indexer/block/fetcher.ex
@@ -13,7 +13,7 @@ defmodule Indexer.Block.Fetcher do
   alias Explorer.Chain
   alias Explorer.Chain.{Address, Block, Hash, Import, Transaction}
   alias Explorer.Chain.Cache.Blocks, as: BlocksCache
-  alias Explorer.Chain.Cache.{BlockNumber, Transactions}
+  alias Explorer.Chain.Cache.{Accounts, BlockNumber, Transactions}
   alias Indexer.Block.Fetcher.Receipts
 
   alias Indexer.Fetcher.{
@@ -176,6 +176,7 @@ defmodule Indexer.Block.Fetcher do
       result = {:ok, %{inserted: inserted, errors: blocks_errors}}
       update_block_cache(inserted[:blocks])
       update_transactions_cache(inserted[:transactions])
+      update_addresses_cache(inserted[:addresses])
       result
     else
       {step, {:error, reason}} -> {:error, {step, reason}}
@@ -196,6 +197,8 @@ defmodule Indexer.Block.Fetcher do
   defp update_transactions_cache(transactions) do
     Transactions.update(transactions)
   end
+
+  defp update_addresses_cache(addresses), do: Accounts.drop_or_update(addresses)
 
   def import(
         %__MODULE__{broadcast: broadcast, callback_module: callback_module} = state,

apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -27,6 +27,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
   alias Ecto.Changeset
   alias EthereumJSONRPC.{FetchedBalances, Subscription}
   alias Explorer.Chain
+  alias Explorer.Chain.Cache.Accounts
   alias Explorer.Counters.AverageBlockTime
   alias Indexer.{Block, Tracer}
   alias Indexer.Block.Realtime.TaskSupervisor
@@ -196,6 +197,8 @@ defmodule Indexer.Block.Realtime.Fetcher do
         %{block_rewards: %{errors: block_reward_errors}},
         json_rpc_named_arguments
       )
+
+      Accounts.drop_or_update(imported[:addresses])
 
       ok
     end

apps/indexer/lib/indexer/fetcher/block_reward.ex
@@ -17,6 +17,7 @@ defmodule Indexer.Fetcher.BlockReward do
   alias EthereumJSONRPC.FetchedBeneficiaries
   alias Explorer.Chain
   alias Explorer.Chain.{Block, Wei}
+  alias Explorer.Chain.Cache.Accounts
   alias Indexer.{BufferedTask, Tracer}
   alias Indexer.Fetcher.BlockReward.Supervisor, as: BlockRewardSupervisor
   alias Indexer.Fetcher.CoinBalance
@@ -130,7 +131,9 @@ defmodule Indexer.Fetcher.BlockReward do
         |> add_gas_payments()
         |> import_block_reward_params()
         |> case do
-          {:ok, %{address_coin_balances: address_coin_balances}} ->
+          {:ok, %{address_coin_balances: address_coin_balances, addresses: addresses}} ->
+            Accounts.drop_or_update(addresses)
+
             CoinBalance.async_fetch_balances(address_coin_balances)
 
             retry_errors(errors)

apps/indexer/lib/indexer/fetcher/coin_balance_on_demand.ex
@@ -19,7 +19,7 @@ defmodule Indexer.Fetcher.CoinBalanceOnDemand do
   alias Explorer.{Chain, Repo}
   alias Explorer.Chain.Address
   alias Explorer.Chain.Address.CoinBalance
-  alias Explorer.Chain.Cache.BlockNumber
+  alias Explorer.Chain.Cache.{Accounts, BlockNumber}
   alias Explorer.Counters.AverageBlockTime
   alias Indexer.Fetcher.CoinBalance, as: CoinBalanceFetcher
   alias Timex.Duration
@@ -71,7 +71,11 @@ defmodule Indexer.Fetcher.CoinBalanceOnDemand do
   end
 
   def handle_cast({:fetch_and_update, block_number, address}, state) do
-    fetch_and_update(block_number, address, state.json_rpc_named_arguments)
+    result = fetch_and_update(block_number, address, state.json_rpc_named_arguments)
+
+    with {:ok, %{addresses: addresses}} <- result do
+      Accounts.drop_or_update(addresses)
+    end
 
     {:noreply, state}
   end

apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -14,6 +14,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
   alias Explorer.Chain
   alias Explorer.Chain.{Block, Hash}
+  alias Explorer.Chain.Cache.Accounts
   alias Indexer.{BufferedTask, Tracer}
   alias Indexer.Transform.Addresses
 
@@ -218,6 +219,8 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
     case imports do
       {:ok, imported} ->
+        Accounts.drop_or_update(imported[:addreses])
+
         async_import_coin_balances(imported, %{
           address_hash_to_fetched_balance_block_number: address_hash_to_block_number
         })

apps/indexer/lib/indexer/fetcher/uncle_block.ex
@@ -12,6 +12,7 @@ defmodule Indexer.Fetcher.UncleBlock do
   alias Ecto.Changeset
   alias EthereumJSONRPC.Blocks
   alias Explorer.Chain
+  alias Explorer.Chain.Cache.Accounts
   alias Explorer.Chain.Hash
   alias Indexer.{Block, BufferedTask, Tracer}
   alias Indexer.Fetcher.UncleBlock
@@ -126,7 +127,8 @@ defmodule Indexer.Fetcher.UncleBlock do
            block_second_degree_relations: %{params: block_second_degree_relations_params},
            transactions: %{params: transactions_params, on_conflict: :nothing}
          }) do
-      {:ok, _} ->
+      {:ok, imported} ->
+        Accounts.drop_or_update(imported[:addresses])
         retry(errors)
 
       {:error, {:import = step, [%Changeset{} | _] = changesets}} ->

dominant-strategies/quai-blockscout

apps/explorer/lib/explorer/chain.ex
@@ -49,6 +49,7 @@ defmodule Explorer.Chain do
   alias Explorer.Chain.Block.{EmissionReward, Reward}
 
   alias Explorer.Chain.Cache.{
+    Accounts,
     BlockCount,
     BlockNumber,
     Blocks,
@@ -1379,6 +1380,36 @@ defmodule Explorer.Chain do
   def list_top_addresses(options \\ []) do
     paging_options = Keyword.get(options, :paging_options, @default_paging_options)
 
+    if is_nil(paging_options.key) do
+      paging_options.page_size
+      |> Accounts.take_enough()
+      |> case do
+        nil ->
+          accounts_with_n = fetch_top_addresses(paging_options)
+
+          accounts_with_n
+          |> Enum.map(fn {address, _n} -> address end)
+          |> Accounts.update()
+
+          accounts_with_n
+
+        accounts ->
+          Enum.map(
+            accounts,
+            &{&1,
+             if is_nil(&1.nonce) do
+               0
+             else
+               &1.nonce + 1
+             end}
+          )
+      end
+    else
+      fetch_top_addresses(paging_options)
+    end
+  end
+
+  defp fetch_top_addresses(paging_options) do
     base_query =
       from(a in Address,
         where: a.fetched_coin_balance > ^0,

apps/indexer/lib/indexer/block/fetcher.ex
@@ -13,7 +13,7 @@ defmodule Indexer.Block.Fetcher do
   alias Explorer.Chain
   alias Explorer.Chain.{Address, Block, Hash, Import, Transaction}
   alias Explorer.Chain.Cache.Blocks, as: BlocksCache
-  alias Explorer.Chain.Cache.{BlockNumber, Transactions}
+  alias Explorer.Chain.Cache.{Accounts, BlockNumber, Transactions}
   alias Indexer.Block.Fetcher.Receipts
 
   alias Indexer.Fetcher.{
@@ -176,6 +176,7 @@ defmodule Indexer.Block.Fetcher do
       result = {:ok, %{inserted: inserted, errors: blocks_errors}}
       update_block_cache(inserted[:blocks])
       update_transactions_cache(inserted[:transactions])
+      update_addresses_cache(inserted[:addresses])
       result
     else
       {step, {:error, reason}} -> {:error, {step, reason}}
@@ -196,6 +197,8 @@ defmodule Indexer.Block.Fetcher do
   defp update_transactions_cache(transactions) do
     Transactions.update(transactions)
   end
+
+  defp update_addresses_cache(addresses), do: Accounts.drop(addresses)
 
   def import(
         %__MODULE__{broadcast: broadcast, callback_module: callback_module} = state,

apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -27,6 +27,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
   alias Ecto.Changeset
   alias EthereumJSONRPC.{FetchedBalances, Subscription}
   alias Explorer.Chain
+  alias Explorer.Chain.Cache.Accounts
   alias Explorer.Counters.AverageBlockTime
   alias Indexer.{Block, Tracer}
   alias Indexer.Block.Realtime.TaskSupervisor
@@ -196,6 +197,8 @@ defmodule Indexer.Block.Realtime.Fetcher do
         %{block_rewards: %{errors: block_reward_errors}},
         json_rpc_named_arguments
       )
+
+      Accounts.drop(imported[:addresses])
 
       ok
     end

apps/indexer/lib/indexer/fetcher/block_reward.ex
@@ -17,6 +17,7 @@ defmodule Indexer.Fetcher.BlockReward do
   alias EthereumJSONRPC.FetchedBeneficiaries
   alias Explorer.Chain
   alias Explorer.Chain.{Block, Wei}
+  alias Explorer.Chain.Cache.Accounts
   alias Indexer.{BufferedTask, Tracer}
   alias Indexer.Fetcher.BlockReward.Supervisor, as: BlockRewardSupervisor
   alias Indexer.Fetcher.CoinBalance
@@ -130,7 +131,9 @@ defmodule Indexer.Fetcher.BlockReward do
         |> add_gas_payments()
         |> import_block_reward_params()
         |> case do
-          {:ok, %{address_coin_balances: address_coin_balances}} ->
+          {:ok, %{address_coin_balances: address_coin_balances, addresses: addresses}} ->
+            Accounts.drop(addresses)
+
             CoinBalance.async_fetch_balances(address_coin_balances)
 
             retry_errors(errors)

apps/indexer/lib/indexer/fetcher/coin_balance_on_demand.ex
@@ -19,7 +19,7 @@ defmodule Indexer.Fetcher.CoinBalanceOnDemand do
   alias Explorer.{Chain, Repo}
   alias Explorer.Chain.Address
   alias Explorer.Chain.Address.CoinBalance
-  alias Explorer.Chain.Cache.BlockNumber
+  alias Explorer.Chain.Cache.{Accounts, BlockNumber}
   alias Explorer.Counters.AverageBlockTime
   alias Indexer.Fetcher.CoinBalance, as: CoinBalanceFetcher
   alias Timex.Duration
@@ -71,7 +71,11 @@ defmodule Indexer.Fetcher.CoinBalanceOnDemand do
   end
 
   def handle_cast({:fetch_and_update, block_number, address}, state) do
-    fetch_and_update(block_number, address, state.json_rpc_named_arguments)
+    result = fetch_and_update(block_number, address, state.json_rpc_named_arguments)
+
+    with {:ok, %{addresses: addresses}} <- result do
+      Accounts.drop(addresses)
+    end
 
     {:noreply, state}
   end

apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -14,6 +14,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
   alias Explorer.Chain
   alias Explorer.Chain.{Block, Hash}
+  alias Explorer.Chain.Cache.Accounts
   alias Indexer.{BufferedTask, Tracer}
   alias Indexer.Transform.Addresses
 
@@ -218,6 +219,8 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
     case imports do
       {:ok, imported} ->
+        Accounts.drop(imported[:addreses])
+
         async_import_coin_balances(imported, %{
           address_hash_to_fetched_balance_block_number: address_hash_to_block_number
         })

apps/indexer/lib/indexer/fetcher/uncle_block.ex
@@ -12,6 +12,7 @@ defmodule Indexer.Fetcher.UncleBlock do
   alias Ecto.Changeset
   alias EthereumJSONRPC.Blocks
   alias Explorer.Chain
+  alias Explorer.Chain.Cache.Accounts
   alias Explorer.Chain.Hash
   alias Indexer.{Block, BufferedTask, Tracer}
   alias Indexer.Fetcher.UncleBlock
@@ -126,7 +127,8 @@ defmodule Indexer.Fetcher.UncleBlock do
            block_second_degree_relations: %{params: block_second_degree_relations_params},
            transactions: %{params: transactions_params, on_conflict: :nothing}
          }) do
-      {:ok, _} ->
+      {:ok, imported} ->
+        Accounts.drop(imported[:addresses])
         retry(errors)
 
       {:error, {:import = step, [%Changeset{} | _] = changesets}} ->