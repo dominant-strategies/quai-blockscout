peachwaravich/blockscout-1

Periodically refresh the table with staking pools (#2557)
The table is refreshed when new staking epoch starts and after an interval
configured in apps/block_scout_web/config/config.exs.
If no interval is configured, the table is only updated on new epoch.

apps/block_scout_web/lib/block_scout_web/notifier.ex
@@ -7,6 +7,7 @@ defmodule BlockScoutWeb.Notifier do
   alias BlockScoutWeb.{AddressContractVerificationView, Endpoint}
   alias Explorer.{Chain, Market, Repo}
   alias Explorer.Chain.{Address, InternalTransaction, Transaction}
+  alias Explorer.Chain.Cache.BlockNumber
   alias Explorer.Counters.AverageBlockTime
   alias Explorer.ExchangeRates.Token
   alias Explorer.SmartContract.{Solidity.CodeCompiler, Solidity.CompilerVersion}
@@ -86,10 +87,12 @@ defmodule BlockScoutWeb.Notifier do
   def handle_event({:chain_event, :staking_update}) do
     epoch_number = ContractState.get(:epoch_number, 0)
     epoch_end_block = ContractState.get(:epoch_end_block, 0)
+    block_number = BlockNumber.max_number()
 
     Endpoint.broadcast("stakes:staking_update", "staking_update", %{
       epoch_number: epoch_number,
-      epoch_end_block: epoch_end_block
+      epoch_end_block: epoch_end_block,
+      block_number: block_number
     })
   end
 

dominant-strategies/quai-blockscout

apps/block_scout_web/lib/block_scout_web/notifier.ex
@@ -7,6 +7,7 @@ defmodule BlockScoutWeb.Notifier do
   alias BlockScoutWeb.{AddressContractVerificationView, Endpoint}
   alias Explorer.{Chain, Market, Repo}
   alias Explorer.Chain.{Address, InternalTransaction, TokenTransfer, Transaction}
+  alias Explorer.Chain.Cache.BlockNumber
   alias Explorer.Chain.Supply.RSK
   alias Explorer.Chain.Transaction.History.TransactionStats
   alias Explorer.Counters.AverageBlockTime
@@ -104,10 +105,12 @@ defmodule BlockScoutWeb.Notifier do
   def handle_event({:chain_event, :staking_update}) do
     epoch_number = ContractState.get(:epoch_number, 0)
     epoch_end_block = ContractState.get(:epoch_end_block, 0)
+    block_number = BlockNumber.get_max()
 
     Endpoint.broadcast("stakes:staking_update", "staking_update", %{
       epoch_number: epoch_number,
-      epoch_end_block: epoch_end_block
+      epoch_end_block: epoch_end_block,
+      block_number: block_number
     })
   end
 