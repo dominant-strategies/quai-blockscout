AleksMVP/blockscout

add partially verified support

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_controller.ex
@@ -10,7 +10,7 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
   alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_full_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       changeset =
@@ -65,7 +65,7 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     json_file = json_files |> Enum.at(0)
 
     if json_file do
-      if Chain.smart_contract_verified?(address_hash_string) do
+      if Chain.smart_contract_full_verified?(address_hash_string) do
         EventsPublisher.broadcast(
           prepare_verification_error(
             "This contract already verified in Blockscout.",
@@ -125,18 +125,20 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     end
   end
 
+  def get_metadata_and_publish(address_hash_string, nil) do
+    case Sourcify.get_metadata(address_hash_string) do
+      {:ok, verification_metadata} ->
+        proccess_metadata_add_publish(address_hash_string, verification_metadata, false)
+
+      {:error, %{"error" => error}} ->
+        {:error, error: error}
+    end
+  end
+
   def get_metadata_and_publish(address_hash_string, conn) do
     case Sourcify.get_metadata(address_hash_string) do
       {:ok, verification_metadata} ->
-        %{"params_to_publish" => params_to_publish, "abi" => abi, "secondary_sources" => secondary_sources} =
-          parse_params_from_sourcify(address_hash_string, verification_metadata)
-
-        ContractController.publish(conn, %{
-          "addressHash" => address_hash_string,
-          "params" => params_to_publish,
-          "abi" => abi,
-          "secondarySources" => secondary_sources
-        })
+        proccess_metadata_add_publish(address_hash_string, verification_metadata, false, conn)
 
       {:error, %{"error" => error}} ->
         EventsPublisher.broadcast(
@@ -144,6 +146,18 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
           :on_demand
         )
     end
+  end
+
+  defp proccess_metadata_add_publish(address_hash_string, verification_metadata, is_partial, conn \\ nil) do
+    %{"params_to_publish" => params_to_publish, "abi" => abi, "secondary_sources" => secondary_sources} =
+      parse_params_from_sourcify(address_hash_string, verification_metadata)
+
+    ContractController.publish(conn, %{
+      "addressHash" => address_hash_string,
+      "params" => Map.put(params_to_publish, "partial_verified", is_partial),
+      "abi" => abi,
+      "secondarySources" => secondary_sources
+    })
   end
 
   def prepare_files_array(files) do
@@ -254,6 +268,33 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     case Integer.parse(runs) do
       {integer, ""} -> integer
       _ -> 200
+    end
+  end
+
+  def check_and_verify(address_hash_string) do
+    if Chain.smart_contract_full_verified?(address_hash_string) do
+      {:ok, :already_full_verified}
+    else
+      if Chain.smart_contract_verified?(address_hash_string) do
+        case Sourcify.check_by_address(address_hash_string) do
+          {:ok, _verified_status} ->
+            get_metadata_and_publish(address_hash_string, nil)
+
+          _ ->
+            {:error, :not_verified}
+        end
+      else
+        case Sourcify.check_by_address_any(address_hash_string) do
+          {:ok, "full", metadata} ->
+            proccess_metadata_add_publish(address_hash_string, metadata, false)
+
+          {:ok, "partial", metadata} ->
+            proccess_metadata_add_publish(address_hash_string, metadata, true)
+
+          _ ->
+            {:error, :not_verified}
+        end
+      end
     end
   end
 end

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_flattened_code_controller.ex
@@ -6,7 +6,7 @@ defmodule BlockScoutWeb.AddressContractVerificationViaFlattenedCodeController do
   alias Explorer.SmartContract.{PublisherWorker, Solidity.CodeCompiler, Solidity.CompilerVersion}
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_full_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       changeset =

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_json_controller.ex
@@ -7,7 +7,7 @@ defmodule BlockScoutWeb.AddressContractVerificationViaJsonController do
   alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_full_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       case Sourcify.check_by_address(address_hash_string) do

apps/explorer/lib/explorer/chain.ex
@@ -3474,6 +3474,71 @@ defmodule Explorer.Chain do
     end
   end
 
+  def update_smart_contract(attrs \\ %{}, external_libraries \\ [], secondary_sources \\ []) do
+    address_hash = Map.get(attrs, :address_hash)
+
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    query_sources =
+      from(
+        source in SmartContractAdditionalSource,
+        where: source.address_hash == ^address_hash
+      )
+
+    _delete_sources = Repo.delete_all(query_sources)
+
+    smart_contract = Repo.one(query)
+
+    smart_contract_changeset =
+      smart_contract
+      |> SmartContract.changeset(attrs)
+      |> Changeset.put_change(:external_libraries, external_libraries)
+
+    new_contract_additional_source = %SmartContractAdditionalSource{}
+
+    smart_contract_additional_sources_changesets =
+      if secondary_sources do
+        secondary_sources
+        |> Enum.map(fn changeset ->
+          new_contract_additional_source
+          |> SmartContractAdditionalSource.changeset(changeset)
+        end)
+      else
+        []
+      end
+
+    # Enforce ShareLocks tables order (see docs: sharelocks.md)
+    insert_contract_query =
+      Multi.new()
+      |> Multi.update(:smart_contract, smart_contract_changeset)
+
+    insert_contract_query_with_additional_sources =
+      smart_contract_additional_sources_changesets
+      |> Enum.with_index()
+      |> Enum.reduce(insert_contract_query, fn {changeset, index}, multi ->
+        Multi.insert(multi, "smart_contract_additional_source_#{Integer.to_string(index)}", changeset)
+      end)
+
+    insert_result =
+      insert_contract_query_with_additional_sources
+      |> Repo.transaction()
+
+    case insert_result do
+      {:ok, %{smart_contract: smart_contract}} ->
+        {:ok, smart_contract}
+
+      {:error, :smart_contract, changeset, _} ->
+        {:error, changeset}
+
+      {:error, :set_address_verified, message, _} ->
+        {:error, message}
+    end
+  end
+
   defp set_address_verified(repo, address_hash) do
     query =
       from(
@@ -3707,20 +3772,54 @@ defmodule Explorer.Chain do
     end
   end
 
-  def smart_contract_verified?(address_hash_str) do
+  def smart_contract_full_verified?(address_hash_str) when is_binary(address_hash_str) do
     case string_to_address_hash(address_hash_str) do
       {:ok, address_hash} ->
-        query =
-          from(
-            smart_contract in SmartContract,
-            where: smart_contract.address_hash == ^address_hash
-          )
-
-        if Repo.one(query), do: true, else: false
+        check_full_verified(address_hash)
 
       _ ->
         false
     end
+  end
+
+  def smart_contract_full_verified?(address_hash) do
+    check_full_verified(address_hash)
+  end
+
+  defp check_full_verified(address_hash) do
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    result = Repo.one(query)
+
+    if result, do: !result.partial_verified
+  end
+
+  def smart_contract_verified?(address_hash_str) when is_binary(address_hash_str) do
+    case string_to_address_hash(address_hash_str) do
+      {:ok, address_hash} ->
+        check_verified(address_hash)
+
+      _ ->
+        false
+    end
+  end
+
+  def smart_contract_verified?(address_hash) do
+    check_verified(address_hash)
+  end
+
+  defp check_verified(address_hash) do
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    if Repo.one(query), do: true, else: false
   end
 
   defp fetch_transactions(paging_options \\ nil) do

apps/explorer/priv/repo/migrations/20210701084814_support_partial_match.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.SupportPartialMatch do
+  use Ecto.Migration
+
+  def change do
+    alter table(:smart_contracts) do
+      add(:partial_verified, :boolean, null: true)
+    end
+  end
+end

dominant-strategies/quai-blockscout

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_controller.ex
@@ -10,7 +10,7 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
   alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_fully_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       changeset =
@@ -65,7 +65,7 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     json_file = json_files |> Enum.at(0)
 
     if json_file do
-      if Chain.smart_contract_verified?(address_hash_string) do
+      if Chain.smart_contract_fully_verified?(address_hash_string) do
         EventsPublisher.broadcast(
           prepare_verification_error(
             "This contract already verified in Blockscout.",
@@ -125,18 +125,20 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     end
   end
 
+  def get_metadata_and_publish(address_hash_string, nil) do
+    case Sourcify.get_metadata(address_hash_string) do
+      {:ok, verification_metadata} ->
+        proccess_metadata_add_publish(address_hash_string, verification_metadata, false)
+
+      {:error, %{"error" => error}} ->
+        {:error, error: error}
+    end
+  end
+
   def get_metadata_and_publish(address_hash_string, conn) do
     case Sourcify.get_metadata(address_hash_string) do
       {:ok, verification_metadata} ->
-        %{"params_to_publish" => params_to_publish, "abi" => abi, "secondary_sources" => secondary_sources} =
-          parse_params_from_sourcify(address_hash_string, verification_metadata)
-
-        ContractController.publish(conn, %{
-          "addressHash" => address_hash_string,
-          "params" => params_to_publish,
-          "abi" => abi,
-          "secondarySources" => secondary_sources
-        })
+        proccess_metadata_add_publish(address_hash_string, verification_metadata, false, conn)
 
       {:error, %{"error" => error}} ->
         EventsPublisher.broadcast(
@@ -144,6 +146,18 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
           :on_demand
         )
     end
+  end
+
+  defp proccess_metadata_add_publish(address_hash_string, verification_metadata, is_partial, conn \\ nil) do
+    %{"params_to_publish" => params_to_publish, "abi" => abi, "secondary_sources" => secondary_sources} =
+      parse_params_from_sourcify(address_hash_string, verification_metadata)
+
+    ContractController.publish(conn, %{
+      "addressHash" => address_hash_string,
+      "params" => Map.put(params_to_publish, "partially_verified", is_partial),
+      "abi" => abi,
+      "secondarySources" => secondary_sources
+    })
   end
 
   def prepare_files_array(files) do
@@ -254,6 +268,33 @@ defmodule BlockScoutWeb.AddressContractVerificationController do
     case Integer.parse(runs) do
       {integer, ""} -> integer
       _ -> 200
+    end
+  end
+
+  def check_and_verify(address_hash_string) do
+    if Chain.smart_contract_fully_verified?(address_hash_string) do
+      {:ok, :already_fully_verified}
+    else
+      if Chain.smart_contract_verified?(address_hash_string) do
+        case Sourcify.check_by_address(address_hash_string) do
+          {:ok, _verified_status} ->
+            get_metadata_and_publish(address_hash_string, nil)
+
+          _ ->
+            {:error, :not_verified}
+        end
+      else
+        case Sourcify.check_by_address_any(address_hash_string) do
+          {:ok, "full", metadata} ->
+            proccess_metadata_add_publish(address_hash_string, metadata, false)
+
+          {:ok, "partial", metadata} ->
+            proccess_metadata_add_publish(address_hash_string, metadata, true)
+
+          _ ->
+            {:error, :not_verified}
+        end
+      end
     end
   end
 end

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_flattened_code_controller.ex
@@ -6,7 +6,7 @@ defmodule BlockScoutWeb.AddressContractVerificationViaFlattenedCodeController do
   alias Explorer.SmartContract.{PublisherWorker, Solidity.CodeCompiler, Solidity.CompilerVersion}
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_fully_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       changeset =

apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_json_controller.ex
@@ -7,7 +7,7 @@ defmodule BlockScoutWeb.AddressContractVerificationViaJsonController do
   alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    if Chain.smart_contract_verified?(address_hash_string) do
+    if Chain.smart_contract_fully_verified?(address_hash_string) do
       redirect(conn, to: address_path(conn, :show, address_hash_string))
     else
       case Sourcify.check_by_address(address_hash_string) do

apps/explorer/lib/explorer/chain.ex
@@ -3479,6 +3479,79 @@ defmodule Explorer.Chain do
     end
   end
 
+  @doc """
+  Updates a `t:SmartContract.t/0`.
+
+  Has the similar logic as create_smart_contract/1.
+  Used in cases when you need to update row in DB contains SmartContract, e.g. in case of changing 
+  status `partially verified` to `fully verified` (re-verify).
+  """
+  @spec update_smart_contract(map()) :: {:ok, SmartContract.t()} | {:error, Ecto.Changeset.t()}
+  def update_smart_contract(attrs \\ %{}, external_libraries \\ [], secondary_sources \\ []) do
+    address_hash = Map.get(attrs, :address_hash)
+
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    query_sources =
+      from(
+        source in SmartContractAdditionalSource,
+        where: source.address_hash == ^address_hash
+      )
+
+    _delete_sources = Repo.delete_all(query_sources)
+
+    smart_contract = Repo.one(query)
+
+    smart_contract_changeset =
+      smart_contract
+      |> SmartContract.changeset(attrs)
+      |> Changeset.put_change(:external_libraries, external_libraries)
+
+    new_contract_additional_source = %SmartContractAdditionalSource{}
+
+    smart_contract_additional_sources_changesets =
+      if secondary_sources do
+        secondary_sources
+        |> Enum.map(fn changeset ->
+          new_contract_additional_source
+          |> SmartContractAdditionalSource.changeset(changeset)
+        end)
+      else
+        []
+      end
+
+    # Enforce ShareLocks tables order (see docs: sharelocks.md)
+    insert_contract_query =
+      Multi.new()
+      |> Multi.update(:smart_contract, smart_contract_changeset)
+
+    insert_contract_query_with_additional_sources =
+      smart_contract_additional_sources_changesets
+      |> Enum.with_index()
+      |> Enum.reduce(insert_contract_query, fn {changeset, index}, multi ->
+        Multi.insert(multi, "smart_contract_additional_source_#{Integer.to_string(index)}", changeset)
+      end)
+
+    insert_result =
+      insert_contract_query_with_additional_sources
+      |> Repo.transaction()
+
+    case insert_result do
+      {:ok, %{smart_contract: smart_contract}} ->
+        {:ok, smart_contract}
+
+      {:error, :smart_contract, changeset, _} ->
+        {:error, changeset}
+
+      {:error, :set_address_verified, message, _} ->
+        {:error, message}
+    end
+  end
+
   defp set_address_verified(repo, address_hash) do
     query =
       from(
@@ -3712,20 +3785,54 @@ defmodule Explorer.Chain do
     end
   end
 
-  def smart_contract_verified?(address_hash_str) do
+  def smart_contract_fully_verified?(address_hash_str) when is_binary(address_hash_str) do
     case string_to_address_hash(address_hash_str) do
       {:ok, address_hash} ->
-        query =
-          from(
-            smart_contract in SmartContract,
-            where: smart_contract.address_hash == ^address_hash
-          )
-
-        if Repo.one(query), do: true, else: false
+        check_fully_verified(address_hash)
 
       _ ->
         false
     end
+  end
+
+  def smart_contract_fully_verified?(address_hash) do
+    check_fully_verified(address_hash)
+  end
+
+  defp check_fully_verified(address_hash) do
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    result = Repo.one(query)
+
+    if result, do: !result.partially_verified
+  end
+
+  def smart_contract_verified?(address_hash_str) when is_binary(address_hash_str) do
+    case string_to_address_hash(address_hash_str) do
+      {:ok, address_hash} ->
+        check_verified(address_hash)
+
+      _ ->
+        false
+    end
+  end
+
+  def smart_contract_verified?(address_hash) do
+    check_verified(address_hash)
+  end
+
+  defp check_verified(address_hash) do
+    query =
+      from(
+        smart_contract in SmartContract,
+        where: smart_contract.address_hash == ^address_hash
+      )
+
+    if Repo.one(query), do: true, else: false
   end
 
   defp fetch_transactions(paging_options \\ nil) do

apps/explorer/priv/repo/migrations/20210701084814_support_partial_match.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.SupportPartialMatch do
+  use Ecto.Migration
+
+  def change do
+    alter table(:smart_contracts) do
+      add(:partially_verified, :boolean, null: true)
+    end
+  end
+end