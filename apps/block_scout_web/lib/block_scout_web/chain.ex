mfornet/blockscout

Rework pagination on the transactions list page

apps/block_scout_web/lib/block_scout_web/chain.ex
@@ -215,6 +215,24 @@ defmodule BlockScoutWeb.Chain do
 
   def paging_options(_params), do: [paging_options: @default_paging_options]
 
+  def fetch_page_number(%{"page_number" => page_number_string}) do
+    case Integer.parse(page_number_string) do
+      {number, ""} ->
+        number
+
+      _ ->
+        1
+    end
+  end
+
+  def fetch_page_number(_), do: 1
+
+  def update_page_numbers(new_page_number, new_page_size, %PagingOptions{} = options) do
+    %PagingOptions{options | page_number: new_page_number, page_size: new_page_size}
+  end
+
+  def update_page_number(_new_page_number, options), do: options
+
   def param_to_block_number(formatted_number) when is_binary(formatted_number) do
     case Integer.parse(formatted_number) do
       {number, ""} -> {:ok, number}

dominant-strategies/quai-blockscout

apps/block_scout_web/lib/block_scout_web/chain.ex
@@ -223,6 +223,22 @@ defmodule BlockScoutWeb.Chain do
     [paging_options: Map.put(paging_options, key, value)]
   end
 
+  def fetch_page_number(%{"page_number" => page_number_string}) do
+    case Integer.parse(page_number_string) do
+      {number, ""} ->
+        number
+
+      _ ->
+        1
+    end
+  end
+
+  def fetch_page_number(_), do: 1
+
+  def update_page_parameters(new_page_number, new_page_size, %PagingOptions{} = options) do
+    %PagingOptions{options | page_number: new_page_number, page_size: new_page_size}
+  end
+
   def param_to_block_number(formatted_number) when is_binary(formatted_number) do
     case Integer.parse(formatted_number) do
       {number, ""} -> {:ok, number}