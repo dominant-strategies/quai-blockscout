videocoin/blockscout

Add view for internal transactions error

apps/explorer/lib/explorer/chain/transaction.ex
@@ -30,8 +30,7 @@ defmodule Explorer.Chain.Transaction do
   alias Explorer.Chain.Transaction.{Fork, Status}
 
   @optional_attrs ~w(max_priority_fee_per_gas max_fee_per_gas block_hash block_number created_contract_address_hash cumulative_gas_used earliest_processing_start
-                     error gas_used index created_contract_code_indexed_at status
-                     to_address_hash revert_reason type)a
+                     error gas_used index created_contract_code_indexed_at status to_address_hash revert_reason type has_error_in_iternal_txs)a
 
   @required_attrs ~w(from_address_hash gas gas_price hash input nonce r s v value)a
 
@@ -135,6 +134,7 @@ defmodule Explorer.Chain.Transaction do
    * `max_priority_fee_per_gas` - User defined maximum fee (tip) per unit of gas paid to validator for transaction prioritization.
    * `max_fee_per_gas` - Maximum total amount per unit of gas a user is willing to pay for a transaction, including base fee and priority fee.
    * `type` - New transaction type identifier introduced in EIP 2718 (Berlin HF)
+   * `has_error_in_iternal_txs` - shows if the internal transactions related to transaction have errors
   """
   @type t :: %__MODULE__{
           block: %Ecto.Association.NotLoaded{} | Block.t() | nil,
@@ -169,7 +169,8 @@ defmodule Explorer.Chain.Transaction do
           revert_reason: String.t() | nil,
           max_priority_fee_per_gas: wei_per_gas | nil,
           max_fee_per_gas: wei_per_gas | nil,
-          type: non_neg_integer() | nil
+          type: non_neg_integer() | nil,
+          has_error_in_iternal_txs: boolean()
         }
 
   @derive {Poison.Encoder,
@@ -234,6 +235,7 @@ defmodule Explorer.Chain.Transaction do
     field(:max_priority_fee_per_gas, Wei)
     field(:max_fee_per_gas, Wei)
     field(:type, :integer)
+    field(:has_error_in_iternal_txs, :boolean)
 
     # A transient field for deriving old block hash during transaction upserts.
     # Used to force refetch of a block in case a transaction is re-collated

apps/explorer/priv/repo/migrations/20211217201759_add_has_error_in_iternal_txs_field_to_transaction.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.AddHasErrorInIternalTxsFieldToTransaction do
+  use Ecto.Migration
+
+  def change do
+    alter table(:transactions) do
+      add(:has_error_in_iternal_txs, :boolean, null: true)
+    end
+  end
+end

apps/explorer/test/explorer/chain/import/runner/internal_transactions_test.exs
@@ -22,6 +22,74 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactionsTest do
       assert :error == Repo.get(Transaction, transaction.hash).status
     end
 
+    test "transaction's has_error_in_iternal_txs become true when its internal_transaction (where index != 0) has an error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_iternal_txs
+
+      index = 0
+      error = nil
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      index = 1
+      error = "Reverted"
+
+      internal_transaction_changes_1 = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes, internal_transaction_changes_1])
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :ok == tx.status
+      assert true == tx.has_error_in_iternal_txs
+    end
+
+    test "transaction's has_error_in_iternal_txs become false when its internal_transaction (where index == 0) has an error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_iternal_txs
+
+      index = 0
+      error = "Reverted"
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes])
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :error == tx.status
+      assert false == tx.has_error_in_iternal_txs
+    end
+
+    test "transaction's has_error_in_iternal_txs become false when its internal_transaction has no error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_iternal_txs
+
+      index = 0
+      error = nil
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      index = 1
+      error = nil
+
+      internal_transaction_changes_1 = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes, internal_transaction_changes_1])
+
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :ok == tx.status
+      assert false == tx.has_error_in_iternal_txs
+    end
+
     test "simple coin transfer's status becomes :error when its internal_transaction has an error" do
       transaction = insert(:transaction) |> with_block(status: :ok)
       insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)

dominant-strategies/quai-blockscout

apps/explorer/lib/explorer/chain/transaction.ex
@@ -30,8 +30,7 @@ defmodule Explorer.Chain.Transaction do
   alias Explorer.Chain.Transaction.{Fork, Status}
 
   @optional_attrs ~w(max_priority_fee_per_gas max_fee_per_gas block_hash block_number created_contract_address_hash cumulative_gas_used earliest_processing_start
-                     error gas_used index created_contract_code_indexed_at status
-                     to_address_hash revert_reason type)a
+                     error gas_used index created_contract_code_indexed_at status to_address_hash revert_reason type has_error_in_internal_txs)a
 
   @required_attrs ~w(from_address_hash gas gas_price hash input nonce r s v value)a
 
@@ -135,6 +134,7 @@ defmodule Explorer.Chain.Transaction do
    * `max_priority_fee_per_gas` - User defined maximum fee (tip) per unit of gas paid to validator for transaction prioritization.
    * `max_fee_per_gas` - Maximum total amount per unit of gas a user is willing to pay for a transaction, including base fee and priority fee.
    * `type` - New transaction type identifier introduced in EIP 2718 (Berlin HF)
+   * `has_error_in_internal_txs` - shows if the internal transactions related to transaction have errors
   """
   @type t :: %__MODULE__{
           block: %Ecto.Association.NotLoaded{} | Block.t() | nil,
@@ -169,7 +169,8 @@ defmodule Explorer.Chain.Transaction do
           revert_reason: String.t() | nil,
           max_priority_fee_per_gas: wei_per_gas | nil,
           max_fee_per_gas: wei_per_gas | nil,
-          type: non_neg_integer() | nil
+          type: non_neg_integer() | nil,
+          has_error_in_internal_txs: boolean()
         }
 
   @derive {Poison.Encoder,
@@ -234,6 +235,7 @@ defmodule Explorer.Chain.Transaction do
     field(:max_priority_fee_per_gas, Wei)
     field(:max_fee_per_gas, Wei)
     field(:type, :integer)
+    field(:has_error_in_internal_txs, :boolean)
 
     # A transient field for deriving old block hash during transaction upserts.
     # Used to force refetch of a block in case a transaction is re-collated

apps/explorer/priv/repo/migrations/20211217201759_add_has_error_in_iternal_txs_field_to_transaction.exs
@@ -1,0 +1,9 @@
+defmodule Explorer.Repo.Migrations.AddHasErrorInInternalTxsFieldToTransaction do
+  use Ecto.Migration
+
+  def change do
+    alter table(:transactions) do
+      add(:has_error_in_internal_txs, :boolean, null: true)
+    end
+  end
+end

apps/explorer/test/explorer/chain/import/runner/internal_transactions_test.exs
@@ -22,6 +22,74 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactionsTest do
       assert :error == Repo.get(Transaction, transaction.hash).status
     end
 
+    test "transaction's has_error_in_internal_txs become true when its internal_transaction (where index != 0) has an error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_internal_txs
+
+      index = 0
+      error = nil
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      index = 1
+      error = "Reverted"
+
+      internal_transaction_changes_1 = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes, internal_transaction_changes_1])
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :ok == tx.status
+      assert true == tx.has_error_in_internal_txs
+    end
+
+    test "transaction's has_error_in_internal_txs become false when its internal_transaction (where index == 0) has an error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_internal_txs
+
+      index = 0
+      error = "Reverted"
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes])
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :error == tx.status
+      assert false == tx.has_error_in_internal_txs
+    end
+
+    test "transaction's has_error_in_internal_txs become false when its internal_transaction has no error" do
+      transaction = insert(:transaction) |> with_block(status: :ok)
+      insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)
+
+      assert :ok == transaction.status
+      assert nil == transaction.has_error_in_internal_txs
+
+      index = 0
+      error = nil
+
+      internal_transaction_changes = make_internal_transaction_changes(transaction, index, error)
+
+      index = 1
+      error = nil
+
+      internal_transaction_changes_1 = make_internal_transaction_changes(transaction, index, error)
+
+      assert {:ok, _} = run_internal_transactions([internal_transaction_changes, internal_transaction_changes_1])
+
+      tx = Repo.get(Transaction, transaction.hash)
+
+      assert :ok == tx.status
+      assert false == tx.has_error_in_internal_txs
+    end
+
     test "simple coin transfer's status becomes :error when its internal_transaction has an error" do
       transaction = insert(:transaction) |> with_block(status: :ok)
       insert(:pending_block_operation, block_hash: transaction.block_hash, fetch_internal_transactions: true)