thesixnetwork/blockscout

Account/Watchlist/Notifications (#13)
* Auth for debug

Fix for Auth callback calls endless cycle

* Logout from auth0 and add profile picture

Fix styles in signin/signout

* Add profile

* Update dockerfiles

* Make auth paths dependent of API_PATH

* Change api_path to network_path

* Save identity from auth into DB

* Hardcode urls to make it work again

Style fixes

* Change sign_in icon

* Create Watchlist together with identity

* Add watchlist show

* Add watchlist addresses

Fix creating account on first login

Make address as it should look like

* Associate watchlist with addresses

* Create watchlist address UI

Add balance in coin (need to use right coin)

* Add tokens on watchlist

Fix warnings

Fix topnav styles

* Add watch_ and notify_ fields in WatchlistAddress

* Notifications draft

Style fixes

Removed divider
Watchlist Address Form:
ERC-721 & ERC-1155 -> NFT
Form style -> modal
Move Auth into Account module

Add any valid address to watchlist

Fix profile namespace in router

Fixes in build Notification

Fixed insert notifications

* Send Emails via SendGrid

* Add AUTH0 & SENDGRID API keys to Makefile

* Finish email variables

Fix warnings

Add links to email

* Update & Delete Watchlist Addresses

Fix styles

Fix warnings

* Authorize create/update watchlist_address

Fix credo warnings

* Refactoring and fix UI

* Introduce sender & template ENVs

* Fix credo warns

* Fix formatter & dialyzer

* Make Notifier main entrypoint

* Fix tests

* Fix comments

* Hardcode auth0 callbacks for prod env

Export it in dev env and it should work:
```
export AUTH0_CALLBACK_URL='http://localhost:4000/auth/auth0/callback'
export AUTH0_LOGOUT_RETURN_URL='http://localhost:4000/auth/logout'
```

* Style fixes

* Private tags on address/transaction/token pages

* Add account logger

* fix email subject

* Fix token_transfers notifications

* Prepare for release

* Fix transaction dropdown

* Remoe unwanted changes

* Reset CI cache

* Fix Dialyzer, gettext

* 

config/config.exs
@@ -32,6 +32,7 @@ config :logger,
     {LoggerFileBackend, :reading_token_functions},
     {LoggerFileBackend, :pending_transactions_to_refetch},
     {LoggerFileBackend, :empty_blocks_to_refetch},
+    {LoggerFileBackend, :account},
     {LoggerFileBackend, :api}
   ]
 

dominant-strategies/quai-blockscout

config/config.exs
@@ -35,7 +35,8 @@ config :logger,
     {LoggerFileBackend, :pending_transactions_to_refetch},
     {LoggerFileBackend, :empty_blocks_to_refetch},
     {LoggerFileBackend, :api},
-    {LoggerFileBackend, :block_import_timings}
+    {LoggerFileBackend, :block_import_timings},
+    {LoggerFileBackend, :account}
   ]
 
 config :logger, :console,